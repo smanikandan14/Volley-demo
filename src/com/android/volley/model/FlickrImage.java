e2311c422e703f224ea64d143c6878e94edfb6c4
smanikandan14 <maniselvaraj@mani-mac.local>
2013-06-23 00:50:10 +0800

    Added GSON parsing.

---
+++ src/com/android/volley/model/FlickrImage.java
@@ -0,0 +1,56 @@
+package com.android.volley.model;
+
+public class FlickrImage {
+
+	
+	String id;
+	
+	String secret;
+	
+	String server;
+	
+	String farm;
+	
+	String title;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSecret() {
+		return secret;
+	}
+
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+
+	public String getServer() {
+		return server;
+	}
+
+	public void setServer(String server) {
+		this.server = server;
+	}
+
+	public String getFarm() {
+		return farm;
+	}
+
+	public void setFarm(String farm) {
+		this.farm = farm;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+}
---
+++ src/com/android/volley/model/FlickrResponse.java
@@ -0,0 +1,13 @@
+package com.android.volley.model;
+
+import java.util.List;
+
+public class FlickrResponse {
+
+	List<FlickrImage> photo;
+
+	public List<FlickrImage> getPhotos() {
+		return photo;
+	}
+	
+}
